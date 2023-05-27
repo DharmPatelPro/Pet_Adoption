1cdd57649ce261fc5b2d044bc9ef711e9893192c
Dharm Patel <108889488+DharmPatelPro@users.noreply.github.com>
2023-05-27 20:44:15 +0530

    dd

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/pet";
+const mongoURI = "mongodb+srv://dharmpatel8160:bjdfLr6JbHesCd2p@cluster0.a5inmhu.mongodb.net/";
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to Mongo Successfully...");
